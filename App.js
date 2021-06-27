c4455c7cfa6d323f99fcceac4156cec535d16cd0
CastyLny <49752325+CastyLny@users.noreply.github.com>
2021-06-27 13:37:41 +0700

    This is a commited file

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, TouchableOpacity} from 'react-native';
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import AppLoading from "expo-app-loading";
+import Bean from "./components/Beans.js"
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let[isFontLoaded] = useFonts({
+    Roboto_400Regular
+  })
+
+
+  var bin = [{key: "0001", title: "banana", description: "Chicken, Bananana, WAWAWA, UWUW"},
+  {key: "0002", title: "banana", description: "Chicken, Bananana, WAWAWA, UWUW"},
+  {key: "0003",title: "banana", description: "Chicken, Bananana, WAWAWA, UWUW"},
+  {key: "0004",title: "banana", description: "Chicken, Bananana, WAWAWA, UWUW"},
+  {key: "0005", title: "banana", description: "Chicken, Bananana, WAWAWA, UWUW"},
+  {key: "0006",title: "banana", description: "Chicken, Bananana, WAWAWA, UWUW"},
+  {key: "0007",title: "banana", description: "Chicken, Bananana, WAWAWA, UWUW"},
+  ]
+
+
+  let [bins, editBin] = useState(bin)
+
+  const addBean = () => {
+    console.log( Math.random() * 1000)
+    bin.push({key: Math.floor(Math.random() * 1000).toString(), title: "bean", description: "Add a description"})
+    editBin(bin)
+  }
+
+  const UpdateBean = (key, title, description) =>{
+    let index = findIndex((item)=>{return item.key == key;})
+    bin[index] = {key:key, title: title, description: description}
+    editBin(bin)
+  }
+
+  if (!isFontLoaded){
+    return <AppLoading/>
+  }else{
+    return (
+      <View style={styles.container}>  
+        <View style={styles.head}>
+          <TouchableOpacity onPress={addBean}><View style={styles.circle}></View></TouchableOpacity>
+          <Text style={styles.headText}>Add a bean</Text>
+        </View>
+        <FlatList contentContainerStyle={styles.beansContainer}
+        data = {bins}
+        renderItem = {({item})=><Bean title = {item.title} key = {item.key} description = {item.description} updateHandler={UpdateBean}/>}/>
+      </View>
+    );
+  }
+  
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container:{
+      paddingTop: 30,
+      backgroundColor: "#F2F4F6",
+      height: "100%",
+    },
+
+    head:{
+      flexDirection: "row",
+      paddingTop: 42
+    },
+
+    circle:{
+      width: 35,
+      height: 35,
+      borderRadius: 30,
+      backgroundColor: "#FFAAA7",
+      marginLeft: 30,
+      marginRight: 18
+
+    },
+    headText:{
+      fontSize: 20,
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    beansContainer:{
+      alignItems: "center",
+      width: "100%"
+    },
+
 });
---
+++ components/Beans.js
@@ -0,0 +1,136 @@
+import React, {useState}from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import { Roboto_400Regular, useFonts } from '@expo-google-fonts/roboto';
+import AppLoading from "expo-app-loading";
+
+
+export default function Bean(props){
+    let [fontLoaded] = useFonts({
+        Roboto_400Regular
+    })
+
+
+    const [isEditTitle, setTitleState] = useState(false)
+    const [isEditDescription, setEditDescription] = useState(false)
+    const [title, setTitle] = useState(props.title)
+    const [description, setDescription] = useState(props.description)
+    const [isOpenDetail, setOpenDetail] = useState (false)
+
+    const HandleCloseInput = () =>{
+        console.log(isEditTitle)
+        setTitleState(!isEditTitle)
+    }
+
+
+    const HandleCloseDescription = () =>{
+        setEditDescription(!isEditDescription)
+    }
+
+    const HandleTitleInput = (text) =>{
+        console.log(text)
+        setTitle(text)
+    }
+
+    const HandleDescriptionInput = (text) =>{
+        setDescription(text)
+    }
+
+    const HandleOpenDetail = () =>{
+        setOpenDetail(!isOpenDetail)
+    }
+
+
+
+    if (!fontLoaded){
+        return <View style={style.beanWrapper}></View>
+    }else{
+        if (isOpenDetail){
+            return(
+                <View style={style.beanWrapper}>
+                    <View style={style.beanHead}>
+                        {isEditTitle?<TextInput style={style.title} 
+                        autoFocus
+                        onBlur={HandleCloseInput} 
+                        onChangeText={(text)=>{HandleTitleInput(text)}}
+                        value = {title}
+                        />
+                        :<Text style={style.title} onPress={HandleCloseInput}>{title} </Text>}
+                        <TouchableOpacity onPress = {HandleOpenDetail}><View style={style.circle} ></View></TouchableOpacity>  
+                    </View>
+                    <View style={style.descriptionWrapper}>
+                        {isEditDescription?<TextInput style={style.description} 
+                        autoFocus
+                        onBlur={HandleCloseDescription} 
+                        onChangeText={(text)=>{HandleDescriptionInput(text)}}
+                        value = {description}
+                        />
+                        :<Text style={style.description} onPress={HandleCloseDescription}>{description} </Text>}
+                    </View>
+                </View>
+                
+            );
+        }else{
+            return(
+                <View style={style.beanWrapper}>
+                    <View style={style.beanHead}>
+                        {isEditTitle?<TextInput style={style.title} 
+                        autoFocus
+                        onBlur={HandleCloseInput} 
+                        onChangeText={(text)=>{HandleTitleInput(text)}}
+                        value = {title}
+                        />
+                        :<Text style={style.title} onPress={HandleCloseInput}>{title} </Text>}
+                        <TouchableOpacity onPress = {HandleOpenDetail}><View style={[style.circle, style.circle2]} ></View></TouchableOpacity>  
+                    </View>
+                </View>
+            );
+        }
+    }
+    
+}
+
+
+const style = StyleSheet.create({
+    beanWrapper:{
+        backgroundColor: "#FFFFFF",
+        width: "100%",
+        padding: 20,
+        borderRadius:22,
+        marginTop: 30,
+        maxHeight: 400
+    },
+    beanHead:{
+        justifyContent: "space-between",
+        flexDirection: "row"
+    },
+    title:{
+        paddingLeft: 20,
+        fontSize: 20,
+        width: "80%",
+        fontWeight: "bold"
+        
+    },
+    circle:{
+        backgroundColor: "#C4E1D9",
+        width: 30,
+        height: 30,
+        borderRadius: 30
+    },
+    circle2:{
+        backgroundColor: "#98DDCA"
+    },
+    descriptionWrapper:{
+        marginTop: 15,
+        backgroundColor: "#F2F4F6",
+        height: "80%",
+        borderRadius: 22,
+        padding: 20,
+    },
+    description:{
+        color: "#747474"
+    }
+
+
+
+
+})
